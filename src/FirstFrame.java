8b5d5ee60224e902d887fe7fb0cf34da14a75955
rinormaloku <rinormaloku37@gmail.com>
2017-07-30 16:27:35 +0200

    * Solving the problem functionally

--- src/FirstFrame.java
+++ src/FirstFrame.java
@@ -2,20 +2,18 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class FirstFrame extends JFrame implements WindowFocusListener {
-    DataObject data = new DataObject();
+public class FirstFrame extends JFrame {
     private JButton openFrame2Button;
     private JLabel jLabelRetrievedValue;
     private JPanel jPanel;
 
     public FirstFrame() throws HeadlessException {
         setContentPane(jPanel);
-        addWindowFocusListener(this);
 
         openFrame2Button.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
-                SecondFrame secondFrame = new SecondFrame(data);
+                SecondFrame secondFrame = new SecondFrame(text -> jLabelRetrievedValue.setText(text));
                 secondFrame.pack();
                 secondFrame.setVisible(true);
             }
@@ -23,21 +21,9 @@ public class FirstFrame extends JFrame implements WindowFocusListener {
     }
 
     public static void main(String[] args) {
-
         FirstFrame firstFrame = new FirstFrame();
         firstFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         firstFrame.pack();
         firstFrame.setVisible(true);
-    }
-
-
-    public void windowGainedFocus(WindowEvent e) {
-        if (data.getName() != null)
-            jLabelRetrievedValue.setText(data.getName());
-    }
-
-    @Override
-    public void windowLostFocus(WindowEvent e) {
-        System.out.println("lost focus");
     }
 }
